9f15333fc17d72c5bfe7329f494f33aa3a1eda9d
FireLord <firelord.xda@gmail.com>
2023-07-05 18:24:12 +0530

    app: basic design of feed page

--- app/src/main/java/com/firelord/growighassignment/presentation/ui/FeedFragment.kt
+++ app/src/main/java/com/firelord/growighassignment/presentation/ui/FeedFragment.kt
@@ -4,16 +4,26 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.FloatRange
 import androidx.fragment.app.Fragment
 import com.firelord.growighassignment.R
+import com.firelord.growighassignment.databinding.FragmentFeedBinding
 
 class FeedFragment : Fragment() {
 
+    private lateinit var feedBinding: FragmentFeedBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_feed, container, false)
+        feedBinding = FragmentFeedBinding.inflate(inflater)
+        return feedBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
     }
 }
