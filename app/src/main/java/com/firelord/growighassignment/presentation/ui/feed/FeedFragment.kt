7bbacfec25463ee2ed1846854c7dc347299b3ed8
FireLord <firelord.xda@gmail.com>
2023-07-31 15:28:08 +0530

    app: fixed lag on feed with rv and nestedScroll

--- app/src/main/java/com/firelord/growighassignment/presentation/ui/feed/FeedFragment.kt
+++ app/src/main/java/com/firelord/growighassignment/presentation/ui/feed/FeedFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.core.view.ViewCompat
 import androidx.core.widget.NestedScrollView
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
@@ -104,6 +105,7 @@ class FeedFragment : Fragment() {
         feedBinding.rvFeed.layoutManager = LinearLayoutManager(activity)
         val itemTouchHelper = ItemTouchHelper(SwipeToShareCallback(photosAdapter,requireContext()))
         itemTouchHelper.attachToRecyclerView(feedBinding.rvFeed)
+        ViewCompat.setNestedScrollingEnabled(feedBinding.rvFeed, false);
     }
 
     private fun viewPhotoList(){
